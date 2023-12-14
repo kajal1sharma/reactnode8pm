6f36d6cb6a13042d057fc349ec8987d159a1a483
kajal <kajal@gmail.com>
2023-12-14 21:06:26 +0530

    ..

--- app2/src/Components/SingleTask.jsx
+++ app2/src/Components/SingleTask.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const SingleTask = ({task}) => {
+const SingleTask = (props) => {
+  let task = props.task;
   return (
     <div style={{marginTop:"10px",textAlign:"center",border:"5px solid pink",color:"white", fontSize:"24px"}}>
       {task}
