a0d9489bb9cc9a3bd7e19adf274df198d25b9bd5
kajal <kajal@gmail.com>
2023-12-28 20:49:29 +0530

    ..

--- tictactoe/src/Tictactoe.jsx
+++ tictactoe/src/Tictactoe.jsx
@@ -5,23 +5,64 @@ const Tictactoe = () => {
      
     const [stateArr, setStateArr] = useState(arr);
     const [player, setPlayer] = useState("X")
+    const [win , setWin] = useState(false)
+    function checkWinner(p){
+        // row 1
+        if(stateArr[0]===p && stateArr[1]===p && stateArr[2]===p){
+          return true;
+        }
+        else if(stateArr[1]===p && stateArr[4]===p && stateArr[5]===p){
+          return true;
+        }
+        else if(stateArr[6]===p && stateArr[7]===p && stateArr[8]===p){
+          return true;
+        }
+        else if(stateArr[0]===p && stateArr[3]===p && stateArr[6]===p){
+          return true;
+        }
+        else if(stateArr[1]===p && stateArr[4]===p && stateArr[7]===p){
+          return true;
+        }
+        else if(stateArr[2]===p && stateArr[5]===p && stateArr[8]===p){
+          return true;
+        }
+        else if(stateArr[0]===p && stateArr[4]===p && stateArr[8]===p){
+          return true;
+        }
+        else if(stateArr[2]===p && stateArr[6]===p && stateArr[4]===p){
+          return true;
+        }
+        else{
+          return false;
+        }
+        
+      }
     return (
     <div style={{display:"grid", 
     gridTemplateColumns:"auto auto auto"}}>
       {stateArr.map(function(ele,ind){
-        return <button
+        return win===false ?<button
                 onClick={function(){
                     console.log("function working")
-                     
-                    stateArr[ind]=player;
-                    setPlayer(player==="X"?"0":"X")
+                    let result =false;
+                     if(stateArr[ind]===".")
+                        {
+                          stateArr[ind]=player;
+                          result = checkWinner(player);
+                          setWin(result);
+                          setPlayer(player==="X"?"0":"X")
+                        }
                     // console.log(stateArr)
-                    setStateArr([...stateArr]) 
+                  
+                    setStateArr([...stateArr])
+                   
+
                 }}
         >
             {ele}
-            </button>
+            </button>:<button>{ele}</button>
       })}
+      {win===true?<p>player {player==="X"?"0":"X"} has won the match</p>:<></>}
     </div>
   )
 }
